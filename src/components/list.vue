eba3949155ccd3570dfd8fac6fd5ea88f8eff95e
franky <lovelyorg@126.com>
2018-12-25 15:18:33 +0800

    添加搜索

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,93 +1,111 @@
 <template>
   <div>
-    <mt-header fixed title="list">
-      <mt-button icon="" slot="right" @click="changeTheKey">change the key</mt-button>
-    </mt-header>
-    <div style="height:28px"></div>
-    <router-link v-for="file in fileList" :to="`/${$route.params.user}/file/${file}`" :key="file" icon="more" slot="right">
-      <mt-cell :title="file"></mt-cell>
+    <mt-search fixed v-model="searchValue" cancel-text="取消" placeholder="搜索">
+    </mt-search>
+    <router-link v-for="file in fileList" :to="`/${$route.params.user}/file/${file.title}`" :key="file.title" icon="more" slot="right">
+      <mt-cell :title="file.title"></mt-cell>
     </router-link>
     <mt-button type="primary" size="small" @click="newFile" style="position: fixed;bottom: 16px;right: 16px;">new</mt-button>
+    <mt-button style="margin-top:120px" @click="changeTheKey" type="danger" size="large">change the key</mt-button>
   </div>
 </template>
 
 <script>
-import { MessageBox } from 'mint-ui'
-import { Toast } from 'mint-ui'
-import axios from 'axios'
-import sessionManager from '../js/sessionManager.js'
-import aes from '../js/aes'
+import { Search } from "mint-ui";
+import { MessageBox } from "mint-ui";
+import { Toast } from "mint-ui";
+import axios from "axios";
+import sessionManager from "../js/sessionManager.js";
+import aes from "../js/aes";
 
 export default {
-  name: 'list',
+  name: "list",
   data() {
     return {
-      fileList: []
-    }
+      fileList: [],
+      originalFilelist: [],
+      searchValue: ""
+    };
   },
   methods: {
     newFile: async function() {
-      let newFilename = ''
+      let newFilename = "";
       try {
-        newFilename = await MessageBox.prompt('输入新文件名')
-        newFilename = newFilename.value
+        newFilename = await MessageBox.prompt("输入新文件名");
+        newFilename = newFilename.value;
 
         if (new RegExp('[\\\\/:*?"<>|.]').test(newFilename))
-          return Toast('文件名不允许特殊字符')
+          return Toast("文件名不允许特殊字符");
       } catch (error) {}
 
-      if (!newFilename) return
+      if (!newFilename) return;
 
       try {
-        var plainttext = sessionManager.getPlainttext()
+        var plainttext = sessionManager.getPlainttext();
         if (plainttext.find(m => m.title == newFilename))
-          return Toast('文件已存在')
-        plainttext.push({ title: newFilename, content: '' })
-        sessionManager.setPlainttext(JSON.stringify(plainttext))
+          return Toast("文件已存在");
+        plainttext.push({ title: newFilename, content: "" });
+        sessionManager.setPlainttext(JSON.stringify(plainttext));
 
-        this.$router.push(`/${this.$route.params.user}/file/${newFilename}`)
+        this.$router.push(`/${this.$route.params.user}/file/${newFilename}`);
       } catch (error) {
-        console.error(error)
-        Toast('网络异常')
+        console.error(error);
+        Toast("网络异常");
       }
     },
     changeTheKey: async function() {
-      let newKey = await MessageBox.prompt('new key')
-      newKey = newKey.value
-      if (!newKey) return
+      let newKey = await MessageBox.prompt("new key");
+      newKey = newKey.value;
+      if (!newKey) return;
       try {
-        var plainttext = sessionManager.getPlainttext()
-        let data = aes.encrypt(JSON.stringify(plainttext), newKey)
-        let ax = aes.decrypt(data, newKey)
+        var plainttext = sessionManager.getPlainttext();
+        let data = aes.encrypt(JSON.stringify(plainttext), newKey);
+        let ax = aes.decrypt(data, newKey);
         let result = await axios.put(`${this.$route.params.user}`, {
           data: data
-        })
-        result = result.data
-        if (result) return Toast(result)
-        sessionManager.setKey(newKey)
-        sessionManager.setPlainttext(JSON.stringify(plainttext))
-        Toast('更新成功')
+        });
+        result = result.data;
+        if (result) return Toast(result);
+        sessionManager.setKey(newKey);
+        sessionManager.setPlainttext(JSON.stringify(plainttext));
+        Toast("更新成功");
       } catch (error) {
-        console.error(error)
-        Toast('更新失败')
+        console.error(error);
+        Toast("更新失败");
       }
     }
   },
   mounted: async function() {
     if (!sessionManager.getKey()) {
-      this.$router.push(`/${this.$route.params.user}`)
+      this.$router.push(`/${this.$route.params.user}`);
     }
-    let data = sessionManager.getPlainttext()
-    this.fileList = data.map(m => {
-      return m.title
-    })
+    let data = sessionManager.getPlainttext();
+    this.originalFilelist = this.fileList = data;
+    this.searchValue = this.$store.state.searchValue;
+  },
+  watch: {
+    searchValue: function(value) {
+      if (!value) return (this.fileList = this.originalFilelist);
+
+      this.fileList = this.originalFilelist.filter(m => {
+        return (
+          new RegExp(`${value}`, "i").test(m.title) ||
+          new RegExp(`${value}`, "i").test(m.content)
+        );
+      });
+      this.$store.commit("searchValue", value);
+    }
   }
-}
+};
 </script>
 
  <style scoped>
 a {
   color: #42b983;
   text-decoration: none;
+}
+
+.mint-search {
+  height: unset;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,16 +3,28 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
+import Vuex from 'vuex'
 import MintUI from "mint-ui";
 import "mint-ui/lib/style.css";
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuex)
 Vue.use(MintUI);
+
+const store = new Vuex.Store({
+  state: {
+    searchValue: ''
+  },
+  mutations: {
+    searchValue: (state, value) => state.searchValue = value,
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
+  store,
   router,
   components: { App },
   template: "<App/>"
